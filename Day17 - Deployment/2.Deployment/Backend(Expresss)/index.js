fd90151764029b0839e02a6bd799f62bd4dce1bc
Shreyas107 <mailto110077@gmail.com>
2025-12-31 17:27:46 +0530

    Day17: Deployment added

---
+++ Day17 - Deployment/2.Deployment/Backend(Expresss)/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) return res.json(err);
+    res.json(result);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`);
+});
